cb2c1cd0c8cafc48e414a1f0d31eab1c8231fae8
이상진 <lee3jjang@gmail.com>
2021-01-10 23:14:08 +0900

    121212

--- 0. 샘플데이터생성_20210103.py
+++ 0. 샘플데이터생성_20210103.py
@@ -1,13 +1,21 @@
 import os
 import numpy as np
-from scipy.stats import lognorm, gamma
+from scipy.stats import lognorm, gamma, nbinom, poisson
 
 # data 폴더 생성
 if not any([s == 'data' for s in os.listdir('.')]): os.mkdir('data')
 
-# 기초데이터
+# 심도 기초데이터 생성
 np.random.seed(20210103)
-# loss = lognorm(s=1, loc=0, scale=10000)
-loss = gamma(a=15, loc=0, scale=10000)
-data = loss.rvs(10000).round().astype(int)
+# severity = lognorm(s=1, loc=0, scale=10000)
+severity = gamma(a=15, loc=0, scale=10000)
+data = severity.rvs(10000).round().astype(int)
 np.savetxt('data/claim.csv', data, fmt='%d', delimiter=',')
+
+# 빈도 기초데이터 생성
+np.random.seed(20210109)
+earned_premium_by_fy = [1000000, 2000000, 1500000, 3000000, 4000000]
+# data = [nbinom(n=exposure*3, p=0.3).rvs() for exposure in earned_premium_by_fy]
+data = [poisson(mu=exposure*4.5/1e6).rvs() for exposure in earned_premium_by_fy]
+data = np.c_[earned_premium_by_fy, data]
+np.savetxt('data/num_of_claims.csv', data, fmt='%d', delimiter=',')
